b46c64276d2ce0736d0892259bda6878211cd1d5
Ruslan <krasiyruslan@gmail.com>
2023-02-06 00:39:55 +0100

    install NEXT, React, TypeScript & Tailwind

---
+++ src/pages/index.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Dash-board</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+        <h1 className="text-6xl font-bold">
+          Welcome to{' '}
+          <a className="text-blue-600" href="https://nextjs.org">
+            Paddel-Dashboard
+          </a>
+        </h1>
+      </main>
+    </div>
+  )
+}
+
+export default Home
